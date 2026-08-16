9f5646215f611c940577f38c53ce7f1c862e3109
Russell Holbrook <russell@friendscale.com>
2026-08-16 16:53:38 -0400

    test: restore original test and explicitly test for all day events

--- test/python/test_calendar.py
+++ test/python/test_calendar.py
@@ -5,6 +5,36 @@ import uICAL
 
 def test_basic_ical1():
     ical = open("test/data/ical_events.txt").read()
+    cal = uICAL.Calendar(ical, begin="20191016T102000Z", end="20191017T103000-0500")
+
+    res = []
+    while cal.next():
+        res.append(cal.current())
+
+    exp = [
+        {
+            'type': 'EVENT',
+            'summary': 'Irrigation Front',
+            'start': (2019, 10, 17, 10, 0, 0, -300),
+            'duration': 1200,
+        },
+        {
+            'type': 'EVENT',
+            'summary': 'Irrigation Back',
+            'start': (2019, 10, 17, 10, 20, 0, -300),
+            'duration': 300,
+        },
+        {
+            'type': 'EVENT',
+            'summary': 'Irrigation Beds',
+            'start': (2019, 10, 17, 10, 30, 0, -300),
+            'duration': 600,
+        },
+    ]
+    assert res == exp
+
+def test_all_day_event():
+    ical = open("test/data/ical_events.txt").read()
     cal = uICAL.Calendar(ical, begin="20191216T102000Z", end="20191217T103000-0500")
 
     res = []
@@ -32,7 +62,6 @@ def test_basic_ical1():
         },
     ]
     assert res == exp
-
 
 def test_basic_range():
     ical = open("test/data/ical_events.txt").read()
