d1086fd0d218fe7b9801a1494c7abaf82745326d
Russell Holbrook <russell@friendscale.com>
2026-09-02 05:10:25 -0400

    All Day events should be exempt from Timezone Awareness checks (#36)
    * Add add-day event to test data illustrate unhandled timezone comparison
    * WIP: Added isDate to DateTime. Tests passing
    * Revert unnecessary whitespace change
    * refactor: rename isDate to dateOnly and add isDate() method for encapsulation and allow const return type
    * test: update event dates and add all-day event test cases
    * fix: initialize dateOnly member variable in DateTime constructors
    * fix: remove redundant dateOnly initialization in non-date-only aware constructors
    * test: restore original test and explicitly test for all day events
    * fix: remove whitespace diff
    * fix: Rename isDate() to isDateOnly()

--- src/uICAL/datetime.h
+++ src/uICAL/datetime.h
@@ -19,13 +19,16 @@ namespace uICAL {
             DateTime(const string& datetime, const TZMap_ptr& tzmap);
             DateTime(const string& datetime, const string& tzid, const TZMap_ptr& tzmap);
             DateTime(const DateStamp& datestamp, const TZ_ptr& tz);
+            DateTime(const DateStamp& datestamp, const TZ_ptr& tz, const bool dateOnly);
             DateTime(seconds_t epochSeconds);
             DateTime(seconds_t epochSeconds, const TZ_ptr& tz);
+            DateTime(seconds_t epochSeconds, const TZ_ptr& tz, const bool dateOnly);
             DateTime(const DateTime&) = default;
 
             void str(ostream& out) const;
 
             bool valid() const;
+            bool isDateOnly() const;
 
             DateStamp datestamp() const;
             DateStamp datestamp(const TZ_ptr& tz) const;
@@ -58,6 +61,7 @@ namespace uICAL {
             void construct(const DateStamp& ds, const TZ_ptr& tz);
             void assert_awareness(const DateTime& other, const string& msg) const;
 
+            bool dateOnly;
             EpochTime epochtime;
     };
 
